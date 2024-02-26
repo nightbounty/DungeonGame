81bf76dffbf36b67a2b05dc450dfab318d920d8e
uhliza <159673450+uhliza@users.noreply.github.com>
2024-02-25 19:25:28 -0500

    Update CharStats.cpp

--- source/CharStats.cpp
+++ source/CharStats.cpp
@@ -1,112 +1,104 @@
-#ifndef CHARSTATS_H
-#define CHARSTATS_H
-#include <string>
+#include "CharStats.h"
 #include <sstream>
 
-//
-class CharStats
-{
-private:
-    int intelligence;
-    int wisdom;
-    int armorClass;
-    int strength;
-    int constitution;
-    int charisma;
-    int dexterity;
-    int attackBonus;
-    int damageBonus;
+// Constructor
+CharStats::CharStats() {
+    intelligence = 0;
+    wisdom = 0;
+    armorClass = 0;
+    strength = 0;
+    constitution = 0;
+    charisma = 0;
+    dexterity = 0;
+    attackBonus = 0;
+    damageBonus = 0;
+}
 
-    // constructor
-public:
-    CharStats()
-    {
-        intelligence = 0;
-        wisdom = 0;
-        armorClass = 0;
-        strength = 0;
-        constitution = 0;
-        wisdom = 0;
-        charisma = 0;
-        dexterity = 0;
-        attackBonus = 0;
-        damageBonus = 0;
-    }
+// Methods to increase each enhancement class
+void CharStats::increaseIntelligence(int amount) {
+    intelligence += amount;
+}
 
-    // methods
-public:
-    void increaseIntelligence(int amount)
-    {
-        intelligence += amount;
-    }
+void CharStats::increaseWisdom(int amount) {
+    wisdom += amount;
+}
 
-    void increaseWisdom(int amount)
-    {
-        wisdom += amount;
-    }
+void CharStats::increaseArmorClass(int amount) {
+    armorClass += amount;
+}
 
-    void increaseArmorClass(int amount)
-    {
-        armorClass += amount;
-    }
+void CharStats::increaseStrength(int amount) {
+    strength += amount;
+}
 
-    void increaseStrength(int amount)
-    {
-        strength += amount;
-    }
+void CharStats::increaseConstitution(int amount) {
+    constitution += amount;
+}
 
-    void increaseConstitution(int amount)
-    {
-        constitution += amount;
-    }
+void CharStats::increaseCharisma(int amount) {
+    charisma += amount;
+}
 
-    void increaseCharisma(int amount)
-    {
-        charisma += amount;
-    }
+void CharStats::increaseAttackBonus(int amount) {
+    attackBonus += amount;
+}
 
-    void increaseAttackBonus(int amount)
-    {
-        attackBonus += amount;
-    }
+void CharStats::increaseDexterity(int amount) {
+    dexterity += amount;
+}
 
-    void increaseDexterity(int amount)
-    {
-        dexterity += amount;
-    }
+void CharStats::increaseDamageBonus(int amount) {
+    damageBonus += amount;
+}
 
-    void increaseDamageBonus(int amount)
-    {
-        damageBonus += amount;
-    }
+// getter methods
+int CharStats::getIntelligence() const {
+    return intelligence;
+}
 
-    // getters
-    int getIntelligence() const { return intelligence; }
-    int getWisdom() const { return wisdom; }
-    int getArmorClass() const { return armorClass; }
-    int getStrength() const { return strength; }
-    int getConstitution() const { return constitution; }
-    int getCharisma() const { return charisma; }
-    int getDexterity() const { return dexterity; }
-    int getAttackBonus() const { return attackBonus; }
-    int getDamageBonus() const { return damageBonus; }
+int CharStats::getWisdom() const {
+    return wisdom;
+}
 
-    // toString method
-    std::string toString() const
-    {
-        std::ostringstream oss;
-        oss << "Intelligence: " << intelligence << "\n"
-            << "Wisdom: " << wisdom << "\n"
-            << "Armor Class: " << armorClass << "\n"
-            << "Strength: " << strength << "\n"
-            << "Constitution: " << constitution << "\n"
-            << "Wisdom: " << wisdom << "\n"
-            << "Charisma: " << charisma << "\n"
-            << "Attack Bonus: "
-            << attackBonus << "\nDexterity: " << dexterity;
+int CharStats::getArmorClass() const {
+    return armorClass;
+}
 
-        return oss.str();
-    }
-};
+int CharStats::getStrength() const {
+    return strength;
+}
 
-#endif // CHARSTATS_H
+int CharStats::getConstitution() const {
+    return constitution;
+}
+
+int CharStats::getCharisma() const {
+    return charisma;
+}
+
+int CharStats::getDexterity() const {
+    return dexterity;
+}
+
+int CharStats::getAttackBonus() const {
+    return attackBonus;
+}
+
+int CharStats::getDamageBonus() const {
+    return damageBonus;
+}
+
+// toString method
+std::string CharStats::toString() const {
+    std::ostringstream oss;
+    oss << "Intelligence: " << intelligence << "\n"
+        << "Wisdom: " << wisdom << "\n"
+        << "Armor Class: " << armorClass << "\n"
+        << "Strength: " << strength << "\n"
+        << "Constitution: " << constitution << "\n"
+        << "Charisma: " << charisma << "\n"
+        << "Dexterity: " << dexterity << "\n"
+        << "Attack Bonus: " << attackBonus << "\n"
+        << "Damage Bonus: " << damageBonus;
+    return oss.str();
+}
