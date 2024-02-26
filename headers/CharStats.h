7be9e0d997440f888b3d49760e452706a50a6ee8
uhliza <159673450+uhliza@users.noreply.github.com>
2024-02-25 19:24:35 -0500

    Update CharStats.h

--- headers/CharStats.h
+++ headers/CharStats.h
@@ -1,38 +1,46 @@
 #ifndef CHARSTATS_H
 #define CHARSTATS_H
 #include <string>
-#include <sstream>
 
-class CharStats
-{
+class CharStats {
 private:
     int intelligence;
     int wisdom;
     int armorClass;
-
+    int strength;
+    int constitution;
+    int charisma;
+    int dexterity;
+    int attackBonus;
+    int damageBonus;
 
 public:
-    // constructor
-    CharStats()
-    {
-    }
+    // Constructor
+    CharStats();
 
-    // Methods that Increases Character Stats
-public:
-    void increaseIntelligence()
-    {
-    }
+    // Methods to Increase the Char's Strength Through The Item
+    void increaseIntelligence(int amount);
+    void increaseWisdom(int amount);
+    void increaseArmorClass(int amount);
+    void increaseStrength(int amount);
+    void increaseConstitution(int amount);
+    void increaseCharisma(int amount);
+    void increaseAttackBonus(int amount);
+    void increaseDexterity(int amount);
+    void increaseDamageBonus(int amount);
 
-    void increaseWisdom()
-    {
-    }
+    // Returns the Int of Each Enhancement class
+    int getIntelligence() const;
+    int getWisdom() const;
+    int getArmorClass() const;
+    int getStrength() const;
+    int getConstitution() const;
+    int getCharisma() const;
+    int getDexterity() const;
+    int getAttackBonus() const;
+    int getDamageBonus() const;
 
-    void increaseArmorClass()
-    {
-    }
-
-    // toString method
     std::string toString() const;
 };
 
-#endif // CHARSTATS_H
+#endif
