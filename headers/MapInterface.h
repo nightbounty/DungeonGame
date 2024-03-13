cd41991b66627ad3aac440dce2e9ac21634ed155
uhliza <159673450+uhliza@users.noreply.github.com>
2024-03-12 22:50:49 -0400

    Update MapInterface.h

--- headers/MapInterface.h
+++ headers/MapInterface.h
@@ -1,4 +1,4 @@
-//Map Interface
+/** @class MapInterface */
 #ifndef MAPINTERFACE_H
 #define MAPINTERFACE_H
 
@@ -8,12 +8,19 @@
 class MapInterface{
 
     public:
+        /**Destructor */
         virtual ~MapInterface(){};
+        /**Method to Read a Map File*/
         virtual void readFile(const std::string &fileName)=0;
+        /**Method: Imports Map Level From Given File*/
         virtual int importMapLevel() = 0;
+        /**Method: Imports World Level From Given File*/
         virtual std::string importWorldName() = 0;
+        /**Method: Imports World Tier (Beginner, Intermediate, Hard, Advanced) From Given File*/
         virtual std::string importWorldTier() = 0;
+        /**Method: Imports Items From Given File*/
         virtual std::vector<Item*> &importItems() = 0;
+        /**Method: Imports Characters From Given File*/
         virtual std::vector<std::string> importCharacters() = 0;
 
 };
